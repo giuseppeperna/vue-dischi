ad5cc2ae0152b60b8a8321f62ff0334196e117c5
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-12-01 15:02:11 +0100

    Vue Dischi
    Bonus completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /**
  @file Stampare a schermo gli album, le cui informazioni sono contenute in una API esterna.
+ Bonus: filtrare la visualizzazione degli album in base al genere.
 
  @author Giuseppe Perna <giuseppeperna.cg@gmail.com>
  */
@@ -9,10 +10,18 @@ const musicApp = new Vue({
     el: '#musicApp',
     data: {
         albums:[],
+        selectedGenre:""
     },
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music").then(result => {
             this.albums = result.data.response;
         })
     },
+    computed: { //Filter album by genre
+        filterAlbum() {
+            return this.albums.filter(albums => {
+              return albums.genre.toLocaleLowerCase().includes(this.selectedGenre.toLowerCase());
+            })
+          },
+    }
 })
